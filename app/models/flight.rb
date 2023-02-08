268eff332f1ab5f92d4f8b13c50ae1bc3b8a0410
Wayne Kenny <waynekenny@Waynes-MacBook-Air.local>
2023-02-08 11:30:21 +1100

    second commit

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,5 +1,5 @@
 class Flight < ApplicationRecord
      has_and_belongs_to_many :seats
      has_and_belongs_to_many :users
-     belongs_to :airplanes
+     belongs_to :airplanes, :optional => true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,51 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+u1 = User.create :user_name => 'jones', :email => 'jonesy@ga.co', :password => 'chicken', :admin => true
+u2 = User.create :user_name => 'craig', :email => 'craigsy@ga.co', :password => 'chicken'
+u3 = User.create :user_name => 'holt', :email => 'holsy@ga.co', :password => 'chicken', :admin => true
+u4 = User.create :user_name => 'burns', :email => 'burnsy@ga.co', :password => 'chicken'
+puts "#{ User.count } users"
+
+Airplane.destroy_all
+a1 = Airplane.create :plane_name => '747', :rows => '30', :columns => '6'
+a2 = Airplane.create :plane_name => '747', :rows => '30', :columns => '6' 
+a3 = Airplane.create :plane_name => '757', :rows => '35', :columns => '6' 
+a4 = Airplane.create :plane_name => '303', :rows => '40', :columns => '6' 
+puts "#{ Airplane.count } airplanes"
+
+Flight.destroy_all
+f1 = Flight.create :flight_number => '23', :origin => 'SYD', :destination => 'MEL', :date => '2023-02-25'
+f2 = Flight.create :flight_number => '12', :origin => 'SYD', :destination => 'LHR', :date => '2023-03-25'
+f3 = Flight.create :flight_number => '45', :origin => 'SYD', :destination => 'PER', :date => '2023-04-25'
+f4 = Flight.create :flight_number => '09', :origin => 'SYD', :destination => 'LGW', :date => '2023-05-20'
+puts "#{ Flight.count } flights"
+
+Seat.destroy_all
+s1 = Seat.create :seat_number => 'A1'
+s2 = Seat.create :seat_number => 'A2'
+s3 = Seat.create :seat_number => 'A3'
+s4 = Seat.create :seat_number => 'A4'
+s5 = Seat.create :seat_number => 'A5'
+s6 = Seat.create :seat_number => 'A6'
+s7 = Seat.create :seat_number => 'B1'
+s8 = Seat.create :seat_number => 'B2'
+s9 = Seat.create :seat_number => 'B3'
+s10 = Seat.create :seat_number => 'B4'
+s11 = Seat.create :seat_number => 'B5'
+s12 = Seat.create :seat_number => 'B6'
+puts "#{ Seat.count } seats"
+
+# Associations
+puts "Flights and users"
+f1.users << u1
+f2.users << u2
+f3.users << u3
+f4.users << u4
+
+puts "Airplanes and seats"
+a1.seats << s1 << s2 << s3 << s4 << s5 << s6
+a2.seats << s7 << s8 << s9 << s10 << s11 << s12
+
+puts "Flights and airplanes"
+a1.flights << f1
+a2.flights << f2
+a3.flights << f3
