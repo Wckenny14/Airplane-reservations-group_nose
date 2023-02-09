8f3fdf154a03a28832cc4f2ebde842cb05ab1838
Wayne Kenny <waynekenny@Waynes-MacBook-Air.local>
2023-02-09 11:42:19 +1100

    thursdays commit rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,21 +32,39 @@ s9 = Seat.create :seat_number => 'B3'
 s10 = Seat.create :seat_number => 'B4'
 s11 = Seat.create :seat_number => 'B5'
 s12 = Seat.create :seat_number => 'B6'
+s13 = Seat.create :seat_number => 'C1'
+s14 = Seat.create :seat_number => 'C2'
+s15 = Seat.create :seat_number => 'C3'
+s16 = Seat.create :seat_number => 'C4'
+s17 = Seat.create :seat_number => 'C5'
+s18 = Seat.create :seat_number => 'C6'
+s19 = Seat.create :seat_number => 'D1'
+s20 = Seat.create :seat_number => 'D2'
+s21 = Seat.create :seat_number => 'D3'
+s22 = Seat.create :seat_number => 'D4'
+s23 = Seat.create :seat_number => 'D5'
+s24 = Seat.create :seat_number => 'D6'
 puts "#{ Seat.count } seats"
 
 # Associations
-puts "Flights and users"
-f1.users << u1
-f2.users << u2
-f3.users << u3
-f4.users << u4
+# puts "Flights and users"
+# f1.users << u1
+# f2.users << u2
+# f3.users << u3
+# f4.users << u4
 
-puts "Airplanes and seats"
-a1.seats << s1 << s2 << s3 << s4 << s5 << s6
-a2.seats << s7 << s8 << s9 << s10 << s11 << s12
+# puts "Airplanes and seats"
+# a1.seats << s1 << s2 << s3 << s4 << s5 << s6
+# a2.seats << s7 << s8 << s9 << s10 << s11 << s12
 
-puts "Flights and airplanes"
-a1.flights << f1
-a2.flights << f2
-a3.flights << f3
-a4.flights << f4
+# puts "Flights and airplanes"
+# a1.flights << f1
+# a2.flights << f2
+# a3.flights << f3
+# a4.flights << f4
+
+puts "Flights and seats"
+f1.seats << s1 << s2 << s3 << s4 << s5 << s6
+f2.seats << s7 << s8 << s9 << s10 << s11 << s12
+f3.seats << s13 << s14 << s18 << s15 << s16 << s17
+f4.seats << s21 << s19 << s20 << s22 << s23 << s24
